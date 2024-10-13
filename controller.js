bf296babdb04498b9b81fc63e9c3d72e89515748
Avan Singh <avansingh085@gmail.com>
2024-10-13 11:52:54 +0530

    second1

--- controller.js
+++ controller.js
@@ -6,7 +6,6 @@ const Product=mongoose.model('Product',ProductSchema);
 //const cartItem=mongoose.model('CartItem',CartItemSchema);
 const jwt=require('jsonwebtoken');
 const bcrypt = require("bcrypt");
-const { ALGORITHMS } = require('twilio/lib/jwt/AccessToken.js');
 const JWT_SECRET="189ac7bcf390094e24315f5302e07ed2d4853d27cbb3f7bdaf532f86d8025fecf3079c8fbcaa1d4166dbd783d84c1a4512e1a89810f77f7ada0a0a39e343ccfb31c066aa9d1dda3fd13f1354425745a7708cfcb6ec94109336327d0797a1e30ace0b77f82be6f1782d96ff1785e3bfe4896e24a480b02129e4ef3076bcf47d9c3bc3f0811550392eed1664ca57c47368ed48f77e9faf124ac517f51015669e9f031b5bf8fbc92a5ca12abb6133b72e0423e1538d5b31fa2209140d6594be451b6bef2da76ccb60d844cdf84a0f303f2b853de19574688a4ada6b9120556ebbd413fcbd7c26c5e7952477dc4b5348124da0c1a5cfd41ef4a8d446c771c70b820f";
 const createOrder=async (req, res) => {
     const options = {
